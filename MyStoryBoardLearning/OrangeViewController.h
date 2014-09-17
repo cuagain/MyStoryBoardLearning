f11d58b4b06ce05d3f8b11881c283c1e502b401a
thanawat.s <thehuks@gmail.com>
2014-09-17 18:23:20 +0700

    testing for unwind segue for navigation

---
+++ MyStoryBoardLearning/OrangeViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrangeViewController.h
+//  MyStoryBoardLearning
+//
+//  Created by thanawat.s on 9/17/2557 BE.
+//  Copyright (c) 2557 thanawat.s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrangeViewController : UIViewController
+
+@end
---
+++ MyStoryBoardLearning/SegueMainNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SegueMainNavigationViewController.h
+//  MyStoryBoardLearning
+//
+//  Created by thanawat.s on 9/17/2557 BE.
+//  Copyright (c) 2557 thanawat.s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegueMainNavigationViewController : UINavigationController
+
+@end
